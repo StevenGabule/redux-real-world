82b318139e40cf93d4b6e6ff3b752f39e500ddf3
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-05-09 21:34:48 +0800

    learn redux real world exmaple

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
+import Root from './containers/Root';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(
+const store = configureStore();
+
+render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Root store={store} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
